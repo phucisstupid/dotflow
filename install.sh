e630ce2a24261efd423e63097db3766a97114890
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-05 23:03:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,5 +13,6 @@ cd dotfiles
 stow -v .
 stow -v zshrc -t ~
 rm -rf .gitignore .stow-local-ignore .stowrc README.md archive.tar.gz .git .gitattributes
-cd zed rm -rf archive.tar.gz
+cd zed 
+rm -rf archive.tar.gz
 echo "✅ Installation complete! All files from ~/dotfiles are now symlinked to ~/.config/"
