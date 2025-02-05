30dd3365cb3090413beee8a50ac99090b61630cb
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-05 23:45:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Go to home directory
 cd ~
+rm -rf dotfiles
 GIT_LFS_SKIP_SMUDGE=1 git clone --depth 1 https://github.com/phucleeuwu/dotfiles.git
 # Remove existing ~/.config and recreate it
 rm -f ~/.zshrc
@@ -15,4 +16,6 @@ stow -v zshrc -t ~
 rm -rf .gitignore .stow-local-ignore .stowrc README.md archive.tar.gz .git .gitattributes
 cd zed 
 rm -rf archive.tar.gz
+source ~/.zshrc
+exit
 echo "✅ Installation complete! All files from ~/dotfiles are now symlinked to ~/.config/"
