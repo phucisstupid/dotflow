35a1bd8d0abc70ebe6d66eb96f101480916b7e74
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-14 09:52:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,20 +19,14 @@ stow -v .
 stow -v zshrc -t ~
 
 # Ask if user wants to remove unwanted files (excluding zed folder but removing its archive.tar.gz)
-read -p "Do you want to remove unwanted files (e.g., .gitignore, .stowrc, and zed/archive.tar.gz)? (y/n) " rm_unwanted
+read -p "Do you want to remove unwanted files (e.g., .gitignore, .stowrc, and raycastconf)? (y/n) " rm_unwanted
 if [[ "$rm_unwanted" =~ ^[Yy]$ ]]; then
-    rm -rf .gitignore .stow-local-ignore .stowrc README.md archive.tar.gz .git .gitattributes
-    if [[ -f "zed/archive.tar.gz" ]]; then
-        rm -rf zed/archive.tar.gz
-    fi
+    rm -rf .gitignore .stow-local-ignore .stowrc README.md raycastconf
     echo "✅ Unwanted files removed."
+else
+    ln -s ~/Documents/Dev/github-copilot ~/.config/github-copilot
+    echo "✅ Symlink created for GitHub Copilot."
 fi
-
-# Apply the new zshrc
-source ~/.zshrc
 
 # Installation complete message
 echo "✅ Installation complete! All files from ~/dotfiles are now symlinked to ~/.config/"
-
-# Exit terminal
-exit
