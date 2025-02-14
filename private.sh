8512ec9e7da59d12668168a41e3804f7a200d50c
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-14 09:26:27 +0700

    Update private.sh

--- private.sh
+++ private.sh
@@ -2,17 +2,12 @@
 set -e
 cd ~
 rm -rf dotfiles
-git lfs install
 git clone --depth 1 https://github.com/phucleeuwu/dotfiles
 cd ~/dotfiles
-git lfs pull
 rm -f ~/.zshrc
 rm -rf ~/.config
 mkdir -p ~/.config
-openssl enc -aes-256-cbc -pbkdf2 -d -in ~/dotfiles/archive.tar.gz | tar -xvf - -C ~/dotfiles
-openssl enc -aes-256-cbc -pbkdf2 -d -in ~/dotfiles/zed/archive.tar.gz | tar -xvf - -C ~/dotfiles/zed
 stow -v .
 stow -v zshrc -t ~
-source ~/.zshrc
 exit
 echo "✅ Installation complete"
