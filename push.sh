1eaef16ce2ce18155cb6d0f9aae45746666a0431
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-14 09:57:12 +0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 cd ~/dotfiles
-brew bundle dump --file=~/dotfiles/Brewfile --force       
+brew bundle dump --file=~/dotfiles/Brewfile --force
+echo "✅ Brewfile updated!"
 lazygit
 echo "✅ Complete!"
