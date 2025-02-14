fb85dd4b20a0db05da46347a39dcea8764ecbfb1
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-02-14 09:25:18 +0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 cd ~/dotfiles
-brew bundle dump --file=~/dotfiles/Brewfile --force
-tar -cf - github-copilot raycast | openssl enc -aes-256-cbc -pbkdf2 -e -out archive.tar.gz
-cd zed
-tar -cf - conversations prompts | openssl enc -aes-256-cbc -pbkdf2 -e -out archive.tar.gz
-cd ~/dotfiles              
+brew bundle dump --file=~/dotfiles/Brewfile --force       
 lazygit
 echo "✅ Complete!"
