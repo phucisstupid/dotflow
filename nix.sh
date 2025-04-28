ed24444be2b52b612cee37d0dcfaddf4cbebc8df
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-04-28 07:38:50 -0700

    Update nix.sh

--- nix.sh
+++ nix.sh
@@ -20,7 +20,7 @@ if ! command -v nix &> /dev/null; then
   source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
-GIT_CMD="nix-shell -p git --"
+GIT_CMD="nix-shell nixpkgs#git --"
 
 # 📁 Set paths
 DOTFILES_DIR="$HOME/dotfiles"
